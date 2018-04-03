af56799cb3a63371d94c2fe7361f965a9a5f98c8
GeniusV <eliot310100@gmail.com>
2018-04-03 11:20:00 +0800

    add User entity

---
+++ src/main/java/com/hjc/entity/User.java
@@ -0,0 +1,43 @@
+package com.hjc.entity;
+
+/**
+ * Created by GeniusV on 4/3/18.
+ */
+public class User {
+    private String useName;
+    private String pwd;
+    private String email;
+    private String tel;
+
+    public String getUseName() {
+        return useName;
+    }
+
+    public void setUseName(String useName) {
+        this.useName = useName;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
