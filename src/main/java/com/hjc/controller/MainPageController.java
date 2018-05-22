d552ac5e2f4c3f86d0b348621eb5780a5d2a5afd
GeniusV <eliot310100@gmail.com>
2018-05-22 15:57:26 +0800

    goods blabla ok

--- src/main/java/com/hjc/controller/MainPageController.java
+++ src/main/java/com/hjc/controller/MainPageController.java
@@ -1,11 +1,15 @@
 package com.hjc.controller;
 
+import com.hjc.entity.Good;
+import com.hjc.service.GoodService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by GeniusV on 5/15/18.
@@ -14,7 +18,10 @@ import java.io.IOException;
 public class MainPageController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
+        GoodService goodService = new GoodService();
+        List<Good> goodList = goodService.getGoodList(100);
+        req.setAttribute("goodList", goodList);
+        req.getRequestDispatcher("./mainPage.jsp").forward(req, resp);
     }
 
     @Override
